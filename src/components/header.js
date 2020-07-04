aa9876d26cb95a06217b1608818f397931a2a98d
karthik.nish <karthik.nishanth06@gmail.com>
2020-07-04 22:36:48 +0530

    final

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import React from "react"
 import logo from "../images/logo-designcode.svg"
 import "./Header.css"
+import StripeCheckout from "react-stripe-checkout"
 // const Header = ({ siteTitle }) => (
 
 // )
@@ -24,6 +25,21 @@ class Header extends React.Component {
       this.setState({ hasScrolled: false })
     }
   }
+  handlePurchase = token => {
+    const amount = 5000
+    const description = "My product"
+    const bodyObject = {
+      tokenId: token.id,
+      email: token.email,
+      name: token.name,
+      description,
+      amount,
+    }
+    fetch("http://localhost:9000/stripe-charge", {
+      method: "POST",
+      body: JSON.stringify(bodyObject),
+    })
+  }
   render() {
     return (
       <div
@@ -36,9 +52,16 @@ class Header extends React.Component {
           <Link to="/courses">Courses</Link>
           <Link to="/downloads">Downloads</Link>
           <Link to="/workshops">Workshops</Link>
-          <Link to="/buy">
+          <StripeCheckout
+            amount={5000}
+            image={logo}
+            token={this.handlePurchase}
+            stripeKey={
+              "pk_test_51H1EiXEv3D34zHMV5vp84nujfmyzO6LnSt4egJr87xEU2jPKltNzABxdTdqkoIIXKdpwaiAcZmv7WphsPeLXNdlX00ecIHDR2g"
+            }
+          >
             <button>Buy</button>
-          </Link>
+          </StripeCheckout>
         </div>
       </div>
     )
